ab916089fbd079b4fe04a50e564c581732b39f15
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-10 15:53:16 +0200

    testing around

--- backgroundcolorgenerator/script.js
+++ backgroundcolorgenerator/script.js
@@ -3,6 +3,15 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+function init() {
+    color1.value = "#FF0000";
+    color2.value = "#FFFF00";
+
+    setGradient();
+}
+
+init();
+
 function setGradient() {
     body.style.background =
     "linear-gradient(to right, "
@@ -13,6 +22,15 @@ function setGradient() {
 
     css.textContent = body.style.background + ";";
 }
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    var col = "rgb(" + r + "," + g + "," + b + ")";
+}
+
+console.log(randomColor());
 
 color1.addEventListener("input", setGradient);
 
