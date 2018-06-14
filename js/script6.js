6a2c0d8f78ba5140c9d7652ed8e17a3cb22b3ab3
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-14 23:36:07 +0200

    some js features, info

---
+++ js/script6.js
@@ -0,0 +1,63 @@
+const a = 1;
+const b = 10;
+const c = 100;
+
+// call stack
+console.log('1');
+setTimeout(() => {
+    console.log('2');
+}, 2000)
+console.log('3');
+
+// Call stack
+
+//Web api
+
+//Callback queue
+
+//Event loop
+
+/* const one = () => {
+    const two = () => {
+        console.log('4');
+    }
+    two();
+}
+one();
+
+// Recursion
+function foo() {
+    foo()
+}
+
+foo(); */
+
+// IIFE
+/* var myApp = {};
+
+(function() {
+    myApp.add = function(a, b) {
+        return a + b;
+    }
+})(); */
+
+// CommonJS + Browserify
+// js1 file1
+/* module.exports = function add (a, b) {
+    return a + b;
+}
+
+// js2 file 2
+var add = require("./add"); */
+
+// ES6+Wepack2
+//js1
+/* export const add = (a, b) => a+b;
+//or
+export default function add() {
+    return a + b;
+}
+// js2
+import { add } from '/.add';
+//or
+import add from './add'; */
