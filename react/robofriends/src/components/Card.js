f7df7b5801effa0f4d2b03a11652e154befbb0ba
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-20 00:21:59 +0200

    corrected size of robots

--- react/robofriends/src/components/Card.js
+++ react/robofriends/src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, email, id }) => {
     // const { name, email, id } = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?200*200`} />
+            <img alt='robots' src={`https://robohash.org/${id}?size=275x275`} />
             <div>
                 {/* <h2>{props.name}</h2> */}
                 <h2>{name}</h2>
