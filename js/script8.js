8b225853cdde207fd78d34ecb0a2296533afb14e
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-20 11:55:53 +0200

    async/await

---
+++ js/script8.js
@@ -0,0 +1,44 @@
+// ASYNC AWAIT
+
+/* movePlayer(100, 'Left')
+    .then(() => movePlayer(400, 'Left'))
+    .then(() => movePlayer(10, 'Right'))
+    .then(() => movePlayer(330, 'Left'))
+
+async function playerStart() {
+    const firstMove = await movePlayer(100, 'Left'); // pause
+    await movePlayer(400, 'Left'); // pause
+    await movePlayer(10, 'Right'); // pause
+    await movePlayer(330, 'Left'); // pause
+} */
+
+/* fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(console.log) */
+
+async function fetchUsers() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await resp.json();
+    console.log(data);
+}
+fetchUsers();
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums',
+]
+
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await Promise.all(urls.map(url => 
+            fetch(url).then(resp => resp.json())
+        ))
+        console.log('users', users);
+        console.log('posts', posts);
+        console.log('albums', albums);
+    } catch(err) {
+        console.log('oops', err);
+    }
+}
+getData();
