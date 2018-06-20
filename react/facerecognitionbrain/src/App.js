b1bc4638e2abe0fff3ca852f82fd5f5fc97c88a0
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-20 22:44:56 +0200

    Start of Clarifai api

--- react/facerecognitionbrain/src/App.js
+++ react/facerecognitionbrain/src/App.js
@@ -6,6 +6,10 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'YOUR_API_KEY'
+ });
+
 const particlesOptions = {
   particles: {
     number: {
@@ -18,6 +22,29 @@ const particlesOptions = {
   }
 }
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+onInputChange = (event) => {
+  console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log('click');
+  app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      // do something with response
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -27,7 +54,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         {/*
         <FaceRecognition /> */}
       </div>
