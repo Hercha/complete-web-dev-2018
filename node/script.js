7ac40712ffdd88483f29b8b0546ef38eab73c0ca
Hercha <jens.hedberg@medieinstitutet.se>
2018-06-25 23:12:29 +0200

    node package nodemon

--- node/script.js
+++ node/script.js
@@ -1,10 +1,12 @@
 const script2 = require('./script2.js');
+//const c = require('http');
 
 const a = script2.largeNumber;
-const b = 5;
+const b = 50;
 
 /* setTimeout(() => {
     console.log(a - b);
 }, 3000) */
 console.log(a + b);
-console.log(__dirname);
+//console.log(__dirname);
+//console.log(c);
